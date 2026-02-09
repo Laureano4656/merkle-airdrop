a654cf0cadba7876c6ab5b7f1e7e6a0dcad5da5d
Laureano4656 <laureanogarciadimartino@gmail.com>
2026-02-09 01:01:21 +0000

    Initial commit

---
+++ src/PizzaToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PizzaToken is ERC20, Ownable {
+    constructor() ERC20("PizzaToken", "PIZZA") Ownable(msg.sender) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
